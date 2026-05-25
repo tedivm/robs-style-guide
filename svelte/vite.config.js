34764be1a78d919da273847b23b32b237285a3d6
Robert Hafner <tedivm@tedivm.com>
2026-05-25 08:43:52 -0500

    fix: move content to root for GitHub Pages

--- svelte/vite.config.js
+++ svelte/vite.config.js
@@ -3,9 +3,9 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [svelte()],
-  base: '/svelte/',
-  build: {
-    outDir: '../docs/svelte',
+   base: '/svelte-built/',
+   build: {
+    outDir: '../svelte-built',
     emptyOutDir: true
   }
 });
